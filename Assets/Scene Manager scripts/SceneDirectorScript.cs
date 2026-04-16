e56823070a78c8febb9aee891510847054d2909f
Becchi2 <96718933+Becchi2@users.noreply.github.com>
2026-04-16 14:23:18 +0200

    morning dialog added fully yayyyy

--- Assets/Scene Manager scripts/SceneDirectorScript.cs
+++ Assets/Scene Manager scripts/SceneDirectorScript.cs
@@ -10,13 +10,14 @@ public class SceneDirectorScript : MonoBehaviour
     CharacterData RefAttraction;// gets the AttractionPoints from CharacterData file and DialogResponse file
 
     void Start()
-    {        
+    {
         StartCoroutine(StartDialogue()); // Start the scene after the narration        
     }
 
     public IEnumerator StartDialogue() // orders the sequence of events in the dialog scene for slitmouth
-    { 
-        Narrator.Narrate(0);
+    {
+        RefAttraction = FindAnyObjectByType<CharacterData>();
+        /*Narrator.Narrate(0);
         yield return new WaitUntil(() => !NarrationManager.Instance.IsNarrationActive()); // wait for Narrate to finish
         Demon.SpeakTo(0); // starts the dialogue at position num in the list of dialogue routes for the demon
         yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive()); // wait for SpeakTo to finish
@@ -30,13 +31,13 @@ public class SceneDirectorScript : MonoBehaviour
         yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
         Narrator.Narrate(3);
         yield return new WaitUntil(() => !NarrationManager.Instance.IsNarrationActive());
-        Demon.SpeakTo(3);
+        Demon.SpeakTo(3); */
         yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
         Narrator.Narrate(4);
         yield return new WaitUntil(() => !NarrationManager.Instance.IsNarrationActive());
         Demon.SpeakTo(4);
         yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
-        RefAttraction = FindAnyObjectByType<CharacterData>();
+        
         if (RefAttraction.Attraction < 3)// the interaction that happens if you say no
         {
             Narrator.Narrate(6);
@@ -49,7 +50,7 @@ public class SceneDirectorScript : MonoBehaviour
 
 
         }
-        else if(RefAttraction.Attraction >= 5) 
+        else if (RefAttraction.Attraction >= 5)
         {
             Demon.SpeakTo(7);
             yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
@@ -59,8 +60,29 @@ public class SceneDirectorScript : MonoBehaviour
             yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
             Narrator.Narrate(8);
             yield return new WaitUntil(() => !NarrationManager.Instance.IsNarrationActive());
+            Demon.SpeakTo(9);
+            yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
+            Narrator.Narrate(9);
+            yield return new WaitUntil(() => !NarrationManager.Instance.IsNarrationActive());
+            Demon.SpeakTo(10);
+            yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
+            Narrator.Narrate(10);
+            yield return new WaitUntil(() => !NarrationManager.Instance.IsNarrationActive());
+
+        }
+        else if (RefAttraction.Attraction  == 4)// the interaction that happens if you say yes
+        {
+            Narrator.Narrate(6);
+            yield return new WaitUntil(() => !NarrationManager.Instance.IsNarrationActive());
+            Demon.SpeakTo(11);
+            yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
+            //enter the fight sequence
+            //either bad ending on or bad ending 2 happens depending on the player's choices in the fight sequence
+
+
+
+
         }
 
     }
-
 }
