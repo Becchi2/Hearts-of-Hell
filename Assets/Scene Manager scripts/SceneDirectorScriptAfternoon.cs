71261736194b026a5b0b8391bf065dd0a7e06e76
Becchi2 <96718933+Becchi2@users.noreply.github.com>
2026-04-17 09:23:53 +0200

    Afternoon lines fully added fucking finally

--- Assets/Scene Manager scripts/SceneDirectorScriptAfternoon.cs
+++ Assets/Scene Manager scripts/SceneDirectorScriptAfternoon.cs
@@ -11,7 +11,7 @@ public class SceneDirectorScriptAfternoon : MonoBehaviour
 
     void Start()
     {
-        StartCoroutine(StartDialogueGood()); // Start the scene after the narration        
+        StartCoroutine(StartDialogueBad()); // Start the scene after the narration        
     }
 
     public IEnumerator StartDialogueGood() // dialogue for the good route of the game, where the player has a high attraction score with the demon
@@ -34,56 +34,90 @@ public class SceneDirectorScriptAfternoon : MonoBehaviour
             yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
 
         }
-            Narrator.Narrate(1);
-            yield return new WaitUntil(() => !NarrationManager.Instance.IsNarrationActive());
-            Demon.SpeakTo(2);
+        Narrator.Narrate(1);
+        yield return new WaitUntil(() => !NarrationManager.Instance.IsNarrationActive());
+        Demon.SpeakTo(2);
+        yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
+
+        if (RefAttraction.Attraction < 5)
+        {
+            Demon.SpeakTo(3);
             yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
 
+        }
+        else if (RefAttraction.Attraction > 5)
+        {
+            Demon.SpeakTo(4);
+            yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
+            Narrator.Narrate(2);
+            yield return new WaitUntil(() => !NarrationManager.Instance.IsNarrationActive());
+            Demon.SpeakTo(5);
+            yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
+            Narrator.Narrate(3);
+            yield return new WaitUntil(() => !NarrationManager.Instance.IsNarrationActive());
+            Demon.SpeakTo(6);
+            yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
             if (RefAttraction.Attraction < 5)
             {
-                Demon.SpeakTo(3);
+                Demon.SpeakTo(7);
                 yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
 
             }
-            else if (RefAttraction.Attraction > 5)
+            else if (RefAttraction.Attraction > 5 && RefAttraction.Attraction < 7)
             {
-                Demon.SpeakTo(4);
+                Demon.SpeakTo(8);
                 yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
-                Narrator.Narrate(2);
+                Narrator.Narrate(4);
                 yield return new WaitUntil(() => !NarrationManager.Instance.IsNarrationActive());
-                Demon.SpeakTo(5);
+
+            }
+            else if (RefAttraction.Attraction > 7)
+            {
+                Demon.SpeakTo(9);
                 yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
-                Narrator.Narrate(3);
-                yield return new WaitUntil(() => !NarrationManager.Instance.IsNarrationActive());
-                Demon.SpeakTo(6);
+
+            }
+
+        }
+    }
+
+    public IEnumerator StartDialogueBad() // dialogue for the bad route of the game, where the player has a low attraction score with the demon
+    {
+        RefAttraction = FindAnyObjectByType<CharacterData>();
+        Narrator.Narrate(0);
+        yield return new WaitUntil(() => !NarrationManager.Instance.IsNarrationActive()); // wait for Narrate to finish*/
+        Demon.SpeakTo(12);
+        yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive()); // wait for SpeakTo to finish
+        if (RefAttraction.Attraction < 5)//if player chooses "just doing my job" choice
+        {
+            Demon.SpeakTo(13);
+            yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
+        }
+        else //if player chooses "not looking for you" choice
+        {
+            Demon.SpeakTo(14);
+            yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
+            Narrator.Narrate(5);
+            yield return new WaitUntil(() => !NarrationManager.Instance.IsNarrationActive());
+            Demon.SpeakTo(15);
+            yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
+            if (RefAttraction.Attraction < 5)
+            {
+                Demon.SpeakTo(16);
                 yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
-                if (RefAttraction.Attraction < 5)
-                {
-                    Demon.SpeakTo(7);
-                    yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
 
-                }
-                else if (RefAttraction.Attraction > 5 && RefAttraction.Attraction < 7)
-                {
-                    Demon.SpeakTo(8);
-                    yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
-                    Narrator.Narrate(4);
-                    yield return new WaitUntil(() => !NarrationManager.Instance.IsNarrationActive());
-
-                }
-                else if (RefAttraction.Attraction > 7)
-                {
-                    Demon.SpeakTo(9);
-                    yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
-
-                }
-
+            }
+            else
+            {
+                Demon.SpeakTo(17);
+                yield return new WaitUntil(() => !DialogueManager.Instance1.IsDialogueActive());
+                Narrator.Narrate(6);
             }
         }
-        
 
 
     }
+}
 
 
     
